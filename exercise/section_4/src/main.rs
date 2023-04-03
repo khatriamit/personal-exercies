95564b2e9c86771e4d8e011011a40a8c33bef7e9
khatriamit <khatriamit054@gmail.com>
2023-04-03 09:11:07 +0545

    🔈stucts and its method using impl

--- exercise/section_4/src/main.rs
+++ exercise/section_4/src/main.rs
@@ -11,6 +11,25 @@ struct Coordinates(i32, i32, i32);
 // Unit struct
 struct UnitStruct;
 
+struct Square {
+    width: u32,
+    height: u32,
+}
+
+impl Square {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn find_width(&self) -> u32 {
+        self.width
+    }
+
+    fn change_width(&mut self, new_width: u32, new_height: u32) {
+        self.width = new_width;
+        self.height = new_height;
+    }
+}
+
 fn main() {
     let user1 = User {
         active: true,
@@ -30,7 +49,21 @@ fn main() {
         user2.username, user2.active, user2.sign_in_count
     );
     let coords = Coordinates(1, 2, 3);
-    println!("The coordinate is {:?}", coords)
+    println!("The coordinate is {:?}", coords);
+
+    let mut square1 = Square {
+        width: 10,
+        height: 10,
+    };
+    println!(
+        "The square's width is {} & height is {}",
+        square1.width, square1.height
+    );
+
+    println!("The area of square is {}", square1.area());
+    println!("The width of square is {}", square1.find_width());
+    square1.change_width(11, 11);
+    println!("The width of square is {}", square1.find_width());
 }
 
 fn build_user(username: String) -> User {
