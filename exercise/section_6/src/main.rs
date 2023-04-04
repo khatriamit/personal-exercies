df6ad8e796c563f2bf28e92267901f20bfc82c79
khatriamit <khatriamit054@gmail.com>
2023-04-04 09:35:38 +0545

    🔔 traits: default and generic impl

--- exercise/section_6/src/main.rs
+++ exercise/section_6/src/main.rs
@@ -1,7 +1,37 @@
+use std::fmt::format;
+
 #[derive(Debug)]
 struct Point<T> {
     x: T,
     y: T,
+}
+
+trait Overview {
+    fn overview(&self) -> String {
+        String::from("This is rust course")
+    }
+}
+
+struct Course {
+    headline: String,
+    author: String,
+}
+
+struct AnotherCourse {
+    headline: String,
+    author: String,
+}
+
+impl Overview for Course {
+    fn overview(&self) -> String {
+        format!("{} {}", self.author, self.headline)
+    }
+}
+
+impl Overview for AnotherCourse {
+    fn overview(&self) -> String {
+        format!("{} {}", self.author, self.headline)
+    }
 }
 fn main() {
     let p1 = Point { x: 1, y: 3 };
@@ -10,4 +40,15 @@ fn main() {
     println!("{:?}", p2);
     let p3 = Point { x: "a", y: "b" };
     println!("{:?}", p3);
+
+    let course1 = Course {
+        headline: String::from("Headline 1"),
+        author: String::from("Alex"),
+    };
+    println!("{}", course1.overview());
+    let course2 = AnotherCourse {
+        headline: String::from("Headline 2"),
+        author: String::from("Bob"),
+    };
+    println!("{}", course2.overview());
 }
