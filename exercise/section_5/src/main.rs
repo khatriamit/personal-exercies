525c38ee67b7675386d1749ca710995e9c06d8c8
khatriamit <khatriamit054@gmail.com>
2023-04-04 08:13:08 +0545

    🔔enum basic types

--- exercise/section_5/src/main.rs
+++ exercise/section_5/src/main.rs
@@ -1,3 +1,44 @@
+enum Pets {
+    Dog,
+    Cat,
+    Fish,
+}
+
+impl Pets {
+    fn what_am_i(self) -> &'static str {
+        match self {
+            Pets::Cat => "I am a cat",
+            Pets::Dog => "I am a dog",
+            Pets::Fish => "I am a fish",
+        }
+    }
+}
+
+enum IpAddrKing {
+    V4(String),
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKing,
+    address: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let dog = Pets::Dog;
+    let cat = Pets::Cat;
+    let fish = Pets::Fish;
+    println!("{}", dog.what_am_i());
+    println!("{}", cat.what_am_i());
+    println!("{}", fish.what_am_i());
+
+    let home = IpAddr {
+        kind: IpAddrKing::V4(String::from("V4")),
+        address: "127.0.0.1".to_owned(),
+    };
+
+    let office = IpAddr {
+        kind: IpAddrKing::V6,
+        address: "127.0.0.1".to_owned(),
+    };
 }
