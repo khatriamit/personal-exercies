83f4789d6b5b16d5f64d06064299cff398d60ec8
khatriamit <khatriamit054@gmail.com>
2023-04-04 09:28:10 +0545

    🔔 generic structs

--- exercise/section_6/src/main.rs
+++ exercise/section_6/src/main.rs
@@ -1,3 +1,13 @@
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
 fn main() {
-    println!("Hello, world!");
+    let p1 = Point { x: 1, y: 3 };
+    println!("{:?}", p1);
+    let p2 = Point { x: 1.2, y: 4.5 };
+    println!("{:?}", p2);
+    let p3 = Point { x: "a", y: "b" };
+    println!("{:?}", p3);
 }
