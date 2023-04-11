ca89e5a24edf62ca179998140b0f30f77f225b29
khatriamit <khatriamit054@gmail.com>
2023-04-11 06:53:47 +0545

    iterators with closures

--- exercise/section_10/src/main.rs
+++ exercise/section_10/src/main.rs
@@ -4,6 +4,29 @@ struct City {
     population: u64,
 }
 
+#[derive(Debug)]
+struct Item {
+    name: String,
+}
+
+#[derive(Debug)]
+struct Range {
+    start: u32,
+    end: u32,
+}
+
+impl std::iter::Iterator for Range {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start >= self.end {
+            return None;
+        }
+        let result = Some(self.start);
+        self.start += 1;
+        result
+    }
+}
+
 fn sort_pop(city: &mut Vec<City>) {
     city.sort_by_key(pop_helper)
 }
@@ -14,6 +37,10 @@ fn pop_helper(pop: &City) -> u64 {
 
 fn sort_pop_closure(pop: &mut Vec<City>) {
     pop.sort_by_key(|p| p.population)
+}
+
+fn check_inventory(items: Vec<Item>, product: String) -> Vec<Item> {
+    items.into_iter().filter(|i| i.name == product).collect()
 }
 
 fn main() {
@@ -49,4 +76,46 @@ fn main() {
 
     sort_pop_closure(&mut vec);
     println!("{:?}", vec);
+
+    let vec = vec![1, 2, 3];
+    for val in vec.iter() {
+        println!("{val}");
+    }
+
+    let vec2 = vec![1, 2, 3, 4, 5];
+    let mut iter = (&vec2).into_iter();
+
+    while let Some(v) = iter.next() {
+        println!("using while:  {v}");
+    }
+
+    let mut vec3 = Vec::new();
+    vec3.push(Item {
+        name: String::from("coat"),
+    });
+    vec3.push(Item {
+        name: String::from("shirt"),
+    });
+    vec3.push(Item {
+        name: String::from("sorts"),
+    });
+    vec3.push(Item {
+        name: String::from("shoes"),
+    });
+
+    let checked = check_inventory(vec3, String::from("shirt"));
+    println!("{:?}", checked);
+
+    let mut range = Range { start: 0, end: 10 };
+    for r in range {
+        println!("custom implement iterator trait: {r}")
+    }
+    let vec4: Vec<u32> = range.filter(|x| x % 2 == 0).collect();
+    println!("using filtered {:?}", vec4);
+}
+
+pub trait Iterator {
+    type Item;
+    fn next(&mut self) -> Option<Self::Item>;
+    // any default methods
 }
