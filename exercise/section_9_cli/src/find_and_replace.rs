441d156b8719c451d139bc16a6ffac88e2ba3bc9
khatriamit <khatriamit054@gmail.com>
2023-04-11 06:07:31 +0545

    find and replace logic

--- exercise/section_9_cli/src/find_and_replace.rs
+++ exercise/section_9_cli/src/find_and_replace.rs
@@ -1,4 +1,6 @@
+use regex::Regex;
 use std::env;
+use std::fs;
 use text_colorizer::*;
 
 #[derive(Debug)]
@@ -16,6 +18,49 @@ pub fn print_help() {
         "Find and Replace".green()
     );
     eprintln!("Usage: <target string> <replacement string> <INPUT FILE> <OUTPUT FILE>");
+}
+
+fn replace(target: &str, replace: &str, data: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(data, replace).to_string())
+}
+
+fn read_and_write(args: &Arguments) {
+    let data = match fs::read_to_string(&args.input_file) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!(
+                "{} failed to read from file {}: {:?}",
+                "Error".red().bold(),
+                args.input_file,
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+    let replace_data = match replace(&args.pattern, &args.replace, &data) {
+        Ok(d) => d,
+        Err(e) => {
+            eprintln!(
+                "{} failed to replace the text: {:?}",
+                "Error".red().bold(),
+                e
+            );
+            std::process::exit(1);
+        }
+    };
+    match fs::write(&args.output_file, &replace_data) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!(
+                "{} failed to write to file {}: {:?}",
+                "Error".red().bold(),
+                args.output_file,
+                e
+            );
+            std::process::exit(1);
+        }
+    }
 }
 
 pub fn parses_args() -> Arguments {
@@ -39,5 +84,6 @@ pub fn parses_args() -> Arguments {
 
 pub fn run() {
     let args = parses_args();
+    read_and_write(&args);
     println!("Args: {:?}", args);
 }
