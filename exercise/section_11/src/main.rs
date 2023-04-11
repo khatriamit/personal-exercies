d5c90f680ef1c017aedc99e2c81abd66419942f7
khatriamit <khatriamit054@gmail.com>
2023-04-11 21:49:53 +0545

    smart pointer reference cell

--- exercise/section_11/src/main.rs
+++ exercise/section_11/src/main.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 fn main() {
     let t = (12, "eggs");
     let b = Box::new(t);
@@ -14,5 +16,12 @@ fn main() {
     assert_eq!(5, x);
     assert_eq!(5, *y);
 
-    println!("{:?}", y)
+    println!("{:?}", y);
+
+    let s1 = Rc::new(String::from("Pointer"));
+    let s2 = s1.clone();
+    let s3 = s2.clone();
+    let s4 = s3.clone();
+
+    println!("{s1} {s2} {s3} {s4}");
 }
